6ceec7868b5d60bdbf819db76f4debbe69fa05b2
Colin Mitchell <muffinista@gmail.com>
2012-04-29 20:24:35 -0400

    parsing tweaks

--- parsing.rb
+++ parsing.rb
@@ -60,10 +60,14 @@ special_info = ""
 sections = {}
 see_also = ""
 
+categories = []
+
 in_body = false
 in_special = false
 current_section = "Introduction"
 in_table = false
+
+
 wikitext.each_line do |line|
   line.chomp!
 
@@ -79,7 +83,9 @@ wikitext.each_line do |line|
 
   puts "#{current_section} #{in_special} #{in_body} #{line}"
 
+
   if in_table
+    # skip tables
     if line.match(/\|\}$/)
       in_table = false
       next
@@ -88,6 +94,9 @@ wikitext.each_line do |line|
     in_table = true
     next
   elsif in_special
+
+    # track the special info that is at the top of an entry, we might use it
+    #
     if line.match(/^\}\}$/)
       in_special = false
       in_body = true
@@ -95,12 +104,18 @@ wikitext.each_line do |line|
       special_info << line << "\n"
       next
     end
+  elsif line.match /\[\[Category\:([^\]]+)\]\]/
+    # store categories on their own
+    categories << $1
   elsif line.match /\=\=([^\=]+)\=\=/
+    # section switch
     current_section = $1
   elsif in_body == false && line.match(/^\{\{Infobox/)
     in_special = true
     next
   else
+
+    # add this line to our current section
     in_body = true
     sections[current_section] ||= ""
     sections[current_section] << line << "\n"
@@ -111,6 +126,7 @@ require 'pp'
 pp sections.keys.inspect
 
 sections.each do |k, text|
+  puts "*** #{k} ***"
   puts clean_text(text)
 end
 exit
